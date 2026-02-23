95cdd773db414a7773c56742a0fe0d1e54aff544
molnartm <molnartm@310-18>
2026-02-23 07:53:03 +0100

    Osztalyok letrehozasa

---
+++ Meretek.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUTOROK
+{
+    public class Meretek
+    {
+        public int szelesseg { get; set; }
+        public int magassag { get; set; }
+        public int melyseg { get; set; }
+
+
+    }
+}
---
+++ Targyak.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUTOROK
+{
+    public class Targyak
+    {
+        public int azonosito { get; set; }
+        public string nev { get; set; }
+        public string anyag { get; set; }
+        public Meretek meretek { get; set; }
+        public int ar { get; set; }
+        public bool keszleten { get; set; }
+    }
+}
